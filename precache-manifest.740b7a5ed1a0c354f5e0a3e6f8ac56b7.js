ec6960bf53cbd4839cd2721a9cc0b9abc909f38c
Oleg Lozovatyi <lozovatyi.jr@gmail.com>
2020-07-08 12:18:31 -0400

    Updates

---
+++ precache-manifest.740b7a5ed1a0c354f5e0a3e6f8ac56b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "456681788f325f8fdd1f32304c33ed2d",
+    "url": "/a-bitter-parent/index.html"
+  },
+  {
+    "revision": "0721a477841ecdcb96c2",
+    "url": "/a-bitter-parent/static/css/main.95196269.chunk.css"
+  },
+  {
+    "revision": "dee6c14d08f3ecc86c77",
+    "url": "/a-bitter-parent/static/js/2.eb0f5bb4.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/a-bitter-parent/static/js/2.eb0f5bb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0721a477841ecdcb96c2",
+    "url": "/a-bitter-parent/static/js/main.e64a86f7.chunk.js"
+  },
+  {
+    "revision": "68dacc4dacde2c5a0721",
+    "url": "/a-bitter-parent/static/js/runtime-main.195d2467.js"
+  },
+  {
+    "revision": "47d11b6e760df2e12b481b7ba1e99387",
+    "url": "/a-bitter-parent/static/media/01d35212.47d11b6e.png"
+  }
+]);
